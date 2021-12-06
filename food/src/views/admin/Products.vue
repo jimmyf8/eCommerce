7cc81965a69d5c4d7218fd27831e867470a0af6d
farescourt <jimmy.farescourt@gmail.com>
2021-12-06 10:24:59 +0100

    products admin

---
+++ food/src/views/admin/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <router-link to="/admin/product-add">add new book</router-link>
+      <ProductList/>
+    
+  </div>
+</template>
+
+<script>
+import ProductList from "@/components/admin/ProductList.vue";
+
+export default {
+    components:{
+        ProductList,
+        
+    }
+}
+</script>
+
+<style>
+
+</style>

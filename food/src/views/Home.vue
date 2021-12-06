f4121eaa1baeb02218808a39cc9edafde048d09e
farescourt <jimmy.farescourt@gmail.com>
2021-12-06 10:25:31 +0100

    home

---
+++ food/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Food Shop</h1>
+    <Products/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Products from '@/views/Products.vue'
+export default {
+  name: "Home",
+  components: {
+    Products
+
+  },
+};
+</script>
+
+

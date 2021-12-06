0df3ceaf83e0dd7296a9c2864a6cc0adae5e55d6
farescourt <jimmy.farescourt@gmail.com>
2021-12-06 10:26:31 +0100

    product list

---
+++ food/src/components/admin/ProductList.vue
@@ -0,0 +1,51 @@
+<template>
+   <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Publisher</th>
+          <th scope="col">Year</th>
+          <th scope="col">Image</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <th >{{product.id}}</th>
+          <td>{{product.title}}</td>
+          <td>{{product.author.firstName}} {{product.author.name}}<td>
+          <td>{{product.publisher}}</td>
+          <td>{{product.year}}</td>
+          <td>{{product.image}}</td>
+          <td>edit</td>
+          <td>delete</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+    created() {
+        this.$store.dispatch("getProducts");   
+    },
+    computed:{
+        products(){
+            return this.$store.state.products;
+        }
+    }
+
+};
+</script>
+
+<style>
+
+</style>

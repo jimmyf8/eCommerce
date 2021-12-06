22521911f49d8ef168db15b88394c19c9a46d51c
farescourt <jimmy.farescourt@gmail.com>
2021-12-06 10:25:18 +0100

    product add

---
+++ food/src/views/admin/ProductAdd.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h3> Create a new book</h3>
+      <ProductForm/>
+  </div>
+</template>
+
+<script>
+import ProductForm from "@/components/admin/ProductForm";
+export default {
+  components:{
+    ProductForm,
+  }
+   
+
+}
+</script>
+
+<style>
+
+</style>
